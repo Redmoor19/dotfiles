04a2ea8bbd596e2c8fcd1a3c1cfb0f54c9d58cae
Redmoor <danilamandrabura@gmail.com>
2025-08-01 16:17:08 +0300

    Telescope setup

--- .config/nvim/lua/redconf/lazy/telescope.lua
+++ .config/nvim/lua/redconf/lazy/telescope.lua
@@ -1,51 +1,35 @@
-return { -- Fuzzy Finder (files, lsp, etc)
+return {
   "nvim-telescope/telescope.nvim",
   event = "VimEnter",
   dependencies = {
     "nvim-lua/plenary.nvim",
-    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+    {
       "nvim-telescope/telescope-fzf-native.nvim",
-
-      -- `build` is used to run some command when the plugin is installed/updated.
-      -- This is only run then, not every time Neovim starts up.
       build = "make",
-
-      -- `cond` is a condition used to determine whether this plugin should be
-      -- installed and loaded.
       cond = function()
         return vim.fn.executable("make") == 1
       end,
     },
     { "nvim-telescope/telescope-ui-select.nvim" },
   },
-  config = function()
-    -- The easiest way to use Telescope, is to start by doing something like:
-    --  :Telescope help_tags
-    --
-    -- After running this command, a window will open up and you're able to
-    -- type in the prompt window. You'll see a list of `help_tags` options and
-    -- a corresponding preview of the help.
-    --
-    -- Two important keymaps to use while in Telescope are:
-    --  - Insert mode: <c-/>
-    --  - Normal mode: ?
-    --
-    -- This opens a window that shows you all of the keymaps for the current
-    -- Telescope picker. This is really useful to discover what Telescope can
-    -- do as well as how to actually do it!
 
-    -- [[ Configure Telescope ]]
-    -- See `:help telescope` and `:help telescope.setup()`
+  config = function()
+    local telescopeConfig = require("telescope.config")
+
+    local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+    table.insert(vimgrep_arguments, "--hidden")
+    table.insert(vimgrep_arguments, "--glob")
+    table.insert(vimgrep_arguments, "!**/.git/*")
+
     require("telescope").setup({
-      -- You can put your default mappings / updates / etc. in here
-      --  All the info you're looking for is in `:help telescope.setup()`
-      --
-      -- defaults = {
-      --   mappings = {
-      --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-      --   },
-      -- },
-      -- pickers = {}
+      defaults = {
+        vimgrep_arguments = vimgrep_arguments,
+      },
+      pickers = {
+      find_files = {
+          find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+        },
+      },
       extensions = {
         ["ui-select"] = {
           require("telescope.themes").get_dropdown(),
@@ -53,20 +37,18 @@ return { -- Fuzzy Finder (files, lsp, etc)
       },
     })
 
-    -- Enable Telescope extensions if they are installed
     pcall(require("telescope").load_extension, "fzf")
     pcall(require("telescope").load_extension, "ui-select")
 
-    -- See `:help telescope.builtin`
     local builtin = require("telescope.builtin")
     vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
     vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
     vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
-    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent"})
-    vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Treesitter"})
-    vim.keymap.set("n", "<leader>fb", builtin.builtin, { desc = "Telescope buildin" })
-
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent" })
+    vim.keymap.set("n", "<leader>ft", builtin.treesitter, { desc = "Treesitter" })
+    vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, { desc = "Ripgrep current buffer" })
+    vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Keymaps" })
   end,
 }
