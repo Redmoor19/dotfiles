87bebd78d9cc679421e679e8c237a2609b882ef7
Redmoor <danilamandrabura@gmail.com>
2025-08-29 13:31:29 +0300

    Configuration updare

--- .config/nvim/lua/redconf/lazy/lsp.lua
+++ .config/nvim/lua/redconf/lazy/lsp.lua
@@ -202,8 +202,8 @@ return {	{
 		-- Some languages (like typescript) have entire language plugins that can be useful:
 		--    https://github.com/pmizio/typescript-tools.nvim
 		--
-		-- But for many setups, the LSP (`ts_ls`) will work just fine
-		ts_ls = {},
+		-- But for many setups, the LSP (`ts_ls`) will work just fine )))))))))))0)))0d0d0d0)))))))00))))))))))))))
+		-- ts_ls = {},
 		--
 		lua_ls = {
 			-- cmd = { ... },
@@ -256,6 +256,4 @@ return {	{
 	})
 		end,
 	},
-	
-
 }
